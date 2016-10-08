73c1b8bcf3f760141efec33d1c6c657b3f408619
Vincenzo Pandolfo <pandolfovince@gmail.com>
2016-10-08 16:56:54 +0100

    report carrier name instead of id

--- skyscanner/FlightInfoStorer.js
+++ skyscanner/FlightInfoStorer.js
@@ -10,16 +10,27 @@ function storePlacesInfo(root) {
     return places;
 }
 
+function storeCarrierInfo(root) {
+  var carriers = {};
+  var allCarriers = root.Carriers;
+  allCarriers.forEach(function (carrier) {
+    carriers[carrier.CarrierId] = carrier.Name;
+  })
+  
+  return carriers;
+}
+
 function storeFlightInfo(root) {
     var allQuotes = root.Quotes;
     var places = storePlacesInfo(root);
+    var carriers = storeCarrierInfo(root);
     var cheapestFlight = new route_finder.Flight(
       places[allQuotes[0].OutboundLeg.OriginId],
       places[allQuotes[0].OutboundLeg.DestinationId],
       allQuotes[0].OutboundLeg.DepartureDate,
       allQuotes[0].OutboundLeg.DepartureDate,
       allQuotes[0].MinPrice,
-      allQuotes[0].OutboundLeg.CarrierIds[0]
+      carriers[allQuotes[0].OutboundLeg.CarrierIds[0]]
     );
 
     allQuotes.forEach(function (quote) {
@@ -31,7 +42,7 @@ function storeFlightInfo(root) {
                 new Date(outboundLeg.DepartureDate),
                 new Date(outboundLeg.DepartureDate),
                 quote.MinPrice,
-                outboundLeg.CarrierIds[0]
+                carriers[outboundLeg.CarrierIds[0]]
             );
         }
     });
